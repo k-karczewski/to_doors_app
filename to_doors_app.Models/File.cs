0d55e05a685875c1a4defa8b75a810e55e143139
Konrad Karczewski <kkarczewski94@gmail.com>
2019-11-10 21:18:41 +0100

    Created model of File

---
+++ to_doors_app.Models/File.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace to_doors_app.Models
+{
+    public class File
+    {
+        /* name of .c file */
+        public string Name { get; set; }
+        /* version of .c file */
+        public string Revision { get; set; }
+        /* list of function names included in file - used only in MTS variant*/
+        public List<string> Functions { get; set; }
+
+        /*first constructor */
+        public File(string name, string revision)
+        {
+            Name = name;
+            Revision = revision;
+        }
+
+        /* second constructor */
+        public File(string name, string revision, List<string> functions)
+        {
+            Name = name;
+            Revision = revision;
+            Functions = functions;
+        }
+    }
+}
