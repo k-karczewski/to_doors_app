037e438e3ab3bdebd7d44cb85c4b915af43baa25
Konrad Karczewski <kkarczewski94@gmail.com>
2019-11-10 21:29:48 +0100

    First version of Module model

---
+++ to_doors_app.Models/Module.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace to_doors_app.Models
+{
+    public class Module
+    {
+        /* property that stores module name */
+        public string Name { get; }
+
+        /* property that stores module label / baseline */
+        public string Baseline { get; }
+
+        /* list that stores files info */
+        public List<File> Files { get; }
+        
+        /* property that stores test report number */
+        public int TrNumber { get; }
+        
+        /* coordinates of module in MTS document */
+        public int RowInMts { get; }
+        public int ColInMts { get; }
+
+        public Module(string name, string baseline, List<File> files, int trNumber, int rowInMts, int colInMts)
+        {
+            Name = name;
+            Baseline = baseline;
+            Files = files;
+            TrNumber = trNumber;
+            RowInMts = rowInMts;
+            ColInMts = colInMts;
+        }
+
+    }
+}
