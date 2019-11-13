7397b68407a86a7ce8cef3524ac0cfc2f1b3dba2
Konrad Karczewski <kkarczewski94@gmail.com>
2019-11-13 22:18:06 +0100

    Created SettingsProvider class

---
+++ to_doors_app.Providers/SettingsProvider/SettingsProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using to_doors_app.Interfaces.Providers;
+
+namespace to_doors_app.Providers.SettingsProvider
+{
+    public static class SettingsProvider
+    {
+
+    }
+}
