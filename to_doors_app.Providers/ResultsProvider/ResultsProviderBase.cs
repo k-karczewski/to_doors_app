8cc239cf268f8df17d38992670aa68b47211784f
Konrad Karczewski <kkarczewski94@gmail.com>
2019-11-18 22:55:49 +0100

    First draft of ResultsProviderBase constructor

--- to_doors_app.Providers/ResultsProvider/ResultsProviderBase.cs
+++ to_doors_app.Providers/ResultsProvider/ResultsProviderBase.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
+using to_doors_app.Interfaces;
 using to_doors_app.Interfaces.Providers;
 using to_doors_app.Models;
-
+using _Settings = to_doors_app.Providers.SettingsProvider.SettingsProvider;
 namespace to_doors_app.Providers.ResultsProvider
 {
     public abstract class ResultsProviderBase : IResultsProviderBase
@@ -13,9 +14,36 @@ namespace to_doors_app.Providers.ResultsProvider
         
         protected string ModuleName { get; } = "";
 
-        protected ResultsProviderBase(string moduleName) 
+        protected ResultsProviderBase(string moduleName, OperationType operationType) 
         {
             ModuleName = moduleName;
+            
+            try
+            {
+                if (!_Settings.container.Equals(null))
+                {
+                    SettingsProvider.Settings operationData = new SettingsProvider.Settings();
+
+                    if (operationType.Equals(OperationType.Unit_Test_Resuls_From_Tessy))
+                    {                        
+                        _Settings.container.OperationTypeData.TryGetValue(OperationType.Unit_Test_Resuls_From_Tessy.ToString(), out operationData);   
+                    }
+                    else
+                    {
+                        _Settings.container.OperationTypeData.TryGetValue(OperationType.Module_Integration_Test_Results_From_Tessy.ToString(), out operationData);
+                    }
+
+                    XmlData = XElement.Load($"{operationData.PathToOverviewReports}{moduleName}{operationData.OverviewReportSufix}");
+                }
+                else
+                {
+                    throw new Exception("Application settings were not loaded before operation of reading xml file");
+                }
+            }
+            catch(Exception ex)
+            {
+                /* show error during reading of xml file*/
+            }
         }
 
         public abstract void FillResultsOfModule(out Module module);
