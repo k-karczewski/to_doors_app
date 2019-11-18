2128b11005ac6e4f4884d8a5acd310ad426573be
Konrad Karczewski <kkarczewski94@gmail.com>
2019-11-18 21:19:34 +0100

    Added enum with operation type interface

---
+++ to_doors_app.Interfaces/IOperationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace to_doors_app.Interfaces
+{
+    public enum OperationType
+    {
+        Unit_Test_Resuls_From_Tessy,
+        Module_Integration_Test_Results_From_Tessy,
+        Test_Specification_From_Module_Test_State
+    }
+}
