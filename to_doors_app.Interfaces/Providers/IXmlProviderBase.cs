471cdc4f71bbc86077e6ab2d9adbefbaf31b16ed
Konrad Karczewski <kkarczewski94@gmail.com>
2019-11-18 22:14:56 +0100

    First draft of XmlProvider

---
+++ to_doors_app.Interfaces/Providers/IXmlProviderBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using to_doors_app.Models;
+
+namespace to_doors_app.Interfaces.Providers
+{
+    public interface IXmlProviderBase
+    {
+        void FillResultsOfModule(out Module module);
+    }
+}
---
+++ to_doors_app.Providers/XmlProvider/XmlProviderBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using to_doors_app.Interfaces.Providers;
+using to_doors_app.Models;
+
+namespace to_doors_app.Providers.XmlProvider
+{
+    public abstract class XmlProviderBase : IXmlProviderBase
+    {
+        protected XElement XmlData { get; set; } = null;
+        
+        protected string ModuleName { get; } = "";
+
+        protected XmlProviderBase(string moduleName) 
+        {
+            ModuleName = moduleName;
+        }
+
+        public abstract void FillResultsOfModule(out Module module);
+
+        protected abstract void LoadXmlFile();
+
+        protected abstract int GetTotalNumberOfTestcases();
+        
+        protected abstract int GetNumberOfSuccesses();
+        
+        protected abstract int GetNumberOfFails();
+        
+        protected abstract int GetNumberOfNotExecuted();
+
+        protected abstract string GetModuleVerdict();
+    }
+}
