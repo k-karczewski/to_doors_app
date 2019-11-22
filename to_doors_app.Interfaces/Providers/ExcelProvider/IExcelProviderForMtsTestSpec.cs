05be2724fc2193134822d00c413af577c8172dd6
Konrad Karczewski <kkarczewski94@gmail.com>
2019-11-22 17:16:29 +0100

    Moved ExcelProvider interfaces to new folder

---
+++ to_doors_app.Interfaces/Providers/ExcelProvider/IExcelProviderForMtsTestSpec.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using to_doors_app.Models;
+
+namespace to_doors_app.Interfaces.Providers
+{
+    public interface IExcelProviderForMtsTestSpec : IExcelProviderBase
+    {
+        List<Module> GetDataOfAllUnitModules();
+    }
+}
