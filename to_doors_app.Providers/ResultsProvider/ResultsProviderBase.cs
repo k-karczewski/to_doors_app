b839467da7d445e3c2279b48a4683d6f495b12b9
Konrad Karczewski <kkarczewski94@gmail.com>
2019-11-24 14:30:47 +0100

    First working version of Results Provider

--- to_doors_app.Providers/ResultsProvider/ResultsProviderBase.cs
+++ to_doors_app.Providers/ResultsProvider/ResultsProviderBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using to_doors_app.Interfaces;
@@ -8,56 +9,76 @@ using to_doors_app.Models;
 using _Settings = to_doors_app.Providers.SettingsProvider.SettingsProvider;
 namespace to_doors_app.Providers.ResultsProvider
 {
-    public abstract class ResultsProviderBase : IResultsProviderBase
+    /* generic base class of Results Provider */
+    public class ResultsProviderBase<T> : IResultsProviderBase<T> where T : Module
     {
-        protected XElement XmlData { get; set; } = null;
-        
-        protected string ModuleName { get; } = "";
+        /* list of files or list of scenarios from xml file (depending on operation type) */
+        protected List<XElement> TestObjectResults { get; set; } = null;
+        /* xml report with all results */
+        protected XElement ResultsReport { get; set; } = null;
 
-        protected ResultsProviderBase(string moduleName, OperationType operationType) 
+        /* public method which fills modules passed by parameter with results stored in xml files */
+        public void FillResultsOfModules(ref List<T> modulesToFillResults, OperationType operationType)
         {
-            ModuleName = moduleName;
-            
-            try
+            /* for every module in list */
+            for (int numberOfModule = 0; numberOfModule < modulesToFillResults.Count; numberOfModule++)
             {
-                if (!_Settings.container.Equals(null))
+                /* load xml file with results  */
+                if(LoadXmlFile(modulesToFillResults[numberOfModule].Name, operationType))
                 {
-                    SettingsProvider.Settings operationData = new SettingsProvider.Settings();
-
-                    if (operationType.Equals(OperationType.Unit_Test_Resuls_From_Tessy))
-                    {                        
-                        _Settings.container.OperationTypeData.TryGetValue(OperationType.Unit_Test_Resuls_From_Tessy.ToString(), out operationData);   
-                    }
-                    else
-                    {
-                        _Settings.container.OperationTypeData.TryGetValue(OperationType.Module_Integration_Test_Results_From_Tessy.ToString(), out operationData);
-                    }
-
-                    XmlData = XElement.Load($"{operationData.PathToOverviewReports}{moduleName}{operationData.OverviewReportSufix}");
+                    FillTestObjectResults(modulesToFillResults[numberOfModule]);
                 }
                 else
                 {
-                    throw new Exception("Application settings were not loaded before operation of reading xml file");
+                    /*error during loading of xml file - stop the loop */
+                    break;
                 }
-            }
-            catch(Exception ex)
-            {
-                /* show error during reading of xml file*/
             }
         }
 
-        public abstract void FillResultsOfModule(out Module module);
+        /* loads xml report with results for module and operation type specified in parameters*/
+        private bool LoadXmlFile(string moduleName, OperationType operationType)
+        {
+            /* try to load xml file*/
+            try
+            {
+                /* if settings are loaded */
+                if (!_Settings.container.Equals(null))
+                {
+                    /* new object of settings */
+                    SettingsProvider.Settings operationData = new SettingsProvider.Settings();
+                    /* get settings from dictionary with key value equal to operation type */
+                    _Settings.container.OperationTypeData.TryGetValue(operationType.ToString(), out operationData);
+                    /* load xml file*/
+                    ResultsReport = XElement.Load($"{operationData.PathToOverviewReports}{moduleName}{operationData.OverviewReportSufix}");
+                    /* get test object results from xml file*/
+                    TestObjectResults = GetTestObjectResults();
+                    /* operation succeeded - return true */
+                    return true;
+                }
+                else
+                {
+                    /* show error */
+                    throw new Exception("Index of module exceeded size of modules to proceed list");
+                }
+            }
+            catch (Exception ex)
+            {
+                /* show error during reading of xml file*/
+            }
+            /* operation failed - return false */
+            return false;
+        }
 
-        protected abstract void LoadXmlFile();
-
-        protected abstract int GetTotalNumberOfTestcases();
-        
-        protected abstract int GetNumberOfSuccesses();
-        
-        protected abstract int GetNumberOfFails();
-        
-        protected abstract int GetNumberOfNotExecuted();
-
-        protected abstract string GetModuleVerdict();
+        /* gets test objects results (files or scenarios) depending on operation type - implementation not needed in base class */
+        protected virtual List<XElement> GetTestObjectResults()
+        {
+            throw new NotImplementedException();
+        }
+        /* sets results to model - implementation not needed in base class*/
+        protected virtual void FillTestObjectResults(T module)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ to_doors_app.Providers/ResultsProvider/UnitsResultsProvider.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using to_doors_app.Interfaces;
+using to_doors_app.Models;
+using _Settings = to_doors_app.Providers.SettingsProvider.SettingsProvider;
+
+namespace to_doors_app.Providers.ResultsProvider
+{
+    /* results provider for unit tests*/
+    public sealed class UnitResultsProvider : ResultsProviderBase<UnitModule>
+    {   
+        /* default constructor */
+        public UnitResultsProvider() { }
+
+        /* fills results of all tested files in module*/
+        protected sealed override void FillTestObjectResults(UnitModule moduleToFillResults)
+        {
+            /* for each test object (File)*/
+            foreach(var fileResults in TestObjectResults)
+            {
+                /* moduleToFillResults has Files property that was created in Excel Provider with MTS data (constains all available .c files)
+                 * now it is time to get only those that were tested in Tessy (that are present in xml file)
+                 * rest of them will have only default data (e.g name, baseline, names of .c file) */
+                File currentFile = moduleToFillResults.Files.FirstOrDefault(x => x.Name.ToLower() == fileResults.Attribute("name").Value.ToLower());
+
+                /* if file was found by its name */
+                if (!currentFile.Equals(null))
+                {
+                    /* object that stores results of test for current .c file */
+                    XElement testcase_statistics = fileResults.Element("testcase_statistics");
+
+                    /* get results from report */
+                    currentFile.NumberOfTestcases = int.Parse(testcase_statistics.Attribute("total").Value);
+
+                    currentFile.NumberOfSuccessfulTestcases = int.Parse(testcase_statistics.Attribute("ok").Value);
+
+                    currentFile.NumberOfFailedTestcases = int.Parse(testcase_statistics.Attribute("notok").Value);
+
+                    currentFile.NumberOfNotExecutedTestcases = int.Parse(testcase_statistics.Attribute("notexecuted").Value);
+
+                    /* try to get reached coverage values */
+                    try
+                    {
+                        currentFile.ValueOfC1Coverage = Convert.ToDouble(fileResults.Element("coverage").Element("c1").Attribute("percentage").Value, CultureInfo.InvariantCulture);
+                        currentFile.ValueOfMcDcCoverage = Convert.ToDouble(fileResults.Element("coverage").Element("mcdc").Attribute("percentage").Value, CultureInfo.InvariantCulture);
+                    }
+                    catch (Exception e)
+                    {
+                        /* show warning that c1/mcdc data is not available*/
+                    }
+
+                    /* evaluate verdict value */
+                    string verdict = testcase_statistics.Attribute("success").Value;
+
+                    if (verdict == "notok")
+                    {
+                        currentFile.Verdict = _Settings.container.NokVerdictValue;
+
+                    }
+                    else if (currentFile.NumberOfTestcases.Equals(0))
+                    {
+                        currentFile.Verdict = _Settings.container.NotTestedVerdictValue;
+                    }
+                    else
+                    {
+                        currentFile.Verdict = _Settings.container.OkVerdictValue;
+                    }
+
+                    /* get results of functions placed in current .c file */
+                    var functionResults = fileResults.Descendants("tessyobject").Where(x => x.Attribute("level").Value == "4");
+
+                    /* list of Functions that will store fillded with results Funcion objects */
+                    List<Function> functions = new List<Function>();
+
+                    /* loop over every tested function */
+                    foreach (var functionResult in functionResults)
+                    {
+                        /* create ready function object */
+                        Function currentFunction = FillFunctionResults(functionResult);
+                        /* add it to the list */
+                        functions.Add(currentFunction);
+                    }
+
+                    /* when list has all functions assign it to the File object */
+                    currentFile.Functions = functions;
+                }
+            }
+        }
+
+        /* gets test objects from xml report*/
+        protected override List<XElement> GetTestObjectResults()
+        {
+            return ResultsReport.Descendants("tessyobject").Where(x => x.Attribute("level").Value == "3").ToList();
+        }
+
+        /* creates Function object with results */
+        private Function FillFunctionResults(XElement functionResult)
+        {
+            /* create new Function object by its name in xml report */
+            Function function = new Function(functionResult.Attribute("name").Value);
+
+            /* load data about testcases created for current function */
+            XElement test_object_statistics = functionResult.Element("testcase_statistics");
+
+            /* get data about testcases - total number, number of "greens", number of "reds" and number of not executed */
+            function.NumberOfTestcases = Convert.ToInt32(test_object_statistics.Attribute("total").Value);
+            function.NumberOfSuccessfulTestcases = Convert.ToInt32(test_object_statistics.Attribute("ok").Value);
+            function.NumberOfFailedTestcases = Convert.ToInt32(test_object_statistics.Attribute("notok").Value);
+            function.NumberOfNotExecutedTestcases = Convert.ToInt32(test_object_statistics.Attribute("notexecuted").Value);
+
+            /* try to read c1 and mc/dc value */
+            /* sometimes it is not possible to run function with mc/dc coverage */
+            try
+            {
+                function.ValueOfC1Coverage = Convert.ToDouble(functionResult.Element("coverage").Element("c1").Attribute("percentage").Value, CultureInfo.InvariantCulture);
+                function.ValueOfMcDcCoverage = Convert.ToDouble(functionResult.Element("coverage").Element("mcdc").Attribute("percentage").Value, CultureInfo.InvariantCulture);
+            }
+            catch
+            {
+                /* show warning */
+                /* to be changed later */
+                Console.WriteLine("Warning: MC/DC value of function " + functionResult.Attribute("name").Value + " is missing in Overview Report");
+            }
+
+            /* evaluate function verdict */
+            string verdict = functionResult.Attribute("success").Value;
+
+            if (verdict.Equals("notok"))
+            {
+                function.Verdict = _Settings.container.NokVerdictValue;
+
+            }
+            else if (function.NumberOfTestcases.Equals(0))
+            {
+                function.Verdict = _Settings.container.NotTestedVerdictValue;
+            }
+            else
+            {
+                function.Verdict = _Settings.container.OkVerdictValue;
+            }
+
+            /* return function*/
+            return function;
+        }
+    }
+}
