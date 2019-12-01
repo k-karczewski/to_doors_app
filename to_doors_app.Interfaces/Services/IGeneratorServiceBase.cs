7b9ef86ba649a907980869256d2e1639e458a573
Konrad Karczewski <kkarczewski94@gmail.com>
2019-12-01 18:30:46 +0100

    Created interface for service

---
+++ to_doors_app.Interfaces/Services/IGeneratorServiceBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using to_doors_app.Models;
+
+namespace to_doors_app.Interfaces.Providers
+{
+    public interface IGeneratorServiceBase
+    {
+        void Run();
+        List<string> GetSheetNames(string pathToMts);
+        void SetSheetName(string sheetName);
+    }
+}
