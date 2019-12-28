32bffde25acbb5e6405a291e44bba2bd85791646
Konrad Karczewski <kkarczewski94@gmail.com>
2019-12-28 13:55:47 +0100

    Integrated ResultsProvider with viewmodel

--- to_doors_app.Providers/ResultsProvider/ResultsProviderBase.cs
+++ to_doors_app.Providers/ResultsProvider/ResultsProviderBase.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Windows;
 using System.Xml.Linq;
-using to_doors_app.Interfaces;
 using to_doors_app.Interfaces.Providers;
 using to_doors_app.Models;
-using _Settings = to_doors_app.Providers.SettingsProvider.SettingsProvider;
+
 namespace to_doors_app.Providers.ResultsProvider
 {
     /* generic base class of Results Provider */
-    public class ResultsProviderBase<T> : IResultsProviderBase<T> where T : Module
+    public abstract class ResultsProviderBase<T> : IResultsProviderBase<T> where T : Module
     {
         /* list of files or list of scenarios from xml file (depending on operation type) */
         protected List<XElement> TestObjectResults { get; set; } = null;
@@ -24,50 +22,27 @@ namespace to_doors_app.Providers.ResultsProvider
             for (int numberOfModule = 0; numberOfModule < modulesToFillResults.Count; numberOfModule++)
             {
                 /* load xml file with results  */
-                if(LoadXmlFile(modulesToFillResults[numberOfModule].Name))
-                {
-                    FillTestObjectResults(modulesToFillResults[numberOfModule]);
-                }
-                else
-                {
-                    /*error during loading of xml file - stop the loop */
-                    break;
-                }
+                LoadXmlFile(modulesToFillResults[numberOfModule].PathToOverviewReport);
+                FillTestObjectResults(modulesToFillResults[numberOfModule]);
             }
         }
 
         /* loads xml report with results for module and operation type specified in parameters*/
-        private bool LoadXmlFile(string moduleName)
+        private void LoadXmlFile(string pathToReport)
         {
             /* try to load xml file*/
             try
             {
-                /* if settings are loaded */
-                if (!_Settings.container.Equals(null))
-                {
-                    /* new object of settings */
-                    SettingsProvider.Settings operationData = new SettingsProvider.Settings();
-                    /* get settings from dictionary with key value equal to operation type */
-                    _Settings.container.OperationTypeData.TryGetValue(_Settings.CurrentOperationType, out operationData);
-                    /* load xml file*/
-                    ResultsReport = XElement.Load($"{operationData.PathToOverviewReports}{moduleName}{operationData.OverviewReportSufix}");
-                    /* get test object results from xml file*/
-                    TestObjectResults = GetTestObjectResults();
-                    /* operation succeeded - return true */
-                    return true;
-                }
-                else
-                {
-                    /* show error */
-                    throw new Exception("Index of module exceeded size of modules to proceed list");
-                }
+                /* load xml file*/
+                ResultsReport = XElement.Load(pathToReport);
+                /* get test object results from xml file*/
+                TestObjectResults = GetTestObjectResults();
             }
             catch (Exception ex)
             {
                 /* show error during reading of xml file*/
+                MessageBox.Show($"Error during reading xml files: {ex}");
             }
-            /* operation failed - return false */
-            return false;
         }
 
         /* gets test objects results (files or scenarios) depending on operation type - implementation not needed in base class */
