2eaf8974da1532ee6a02a2dcd44c47feef8c85e9
Konrad Karczewski <kkarczewski94@gmail.com>
2020-01-12 12:13:36 +0100

    Added documentation comments

--- to_doors_app.ViewModels/Helpers/GeneratorViewModelHelpers.cs
+++ to_doors_app.ViewModels/Helpers/GeneratorViewModelHelpers.cs
@@ -12,14 +12,22 @@ namespace to_doors_app.ViewModels.Helpers
 {
     public class GeneratorViewModelHelpers
     {
+        /// <summary>
+        /// Refreshes viewmodel if propertyChanged parameter is not null 
+        /// </summary>
+        /// <param name="model">sender object</param>
+        /// <param name="propertyChanged">event handler</param>
+        /// <param name="nameOfField">name of property that is needed to refresh</param>
         public static void RefreshViewModel(GeneratorViewModel model, PropertyChangedEventHandler propertyChanged, string nameOfField)
         {
-            if(propertyChanged != null)
-            {
-                propertyChanged.Invoke(model, new PropertyChangedEventArgs(nameOfField));
-            }
+            //if not null invoke event listener
+            propertyChanged?.Invoke(model, new PropertyChangedEventArgs(nameOfField));
         }
 
+        /// <summary>
+        /// Gets ovierview reports' paths
+        /// </summary>
+        /// <returns>List of paths to overview reports</returns>
         public static List<string> GetOverviewReportPaths()
         {
             List<string> currentPaths = null;
@@ -50,17 +58,23 @@ namespace to_doors_app.ViewModels.Helpers
             return currentPaths;
         }
 
+        /// <summary>
+        /// This helper method opens open file dialog and allows picking of .xlsx document
+        /// </summary>
+        /// <returns>Path to module test state document (excel) or empty string when user canceled operation</returns>
         public static string GetMtsFilePath()
         {
             string currentPath = string.Empty;
 
             try
             {
+                // Create dialog
                 CommonOpenFileDialog dialog = new CommonOpenFileDialog();
                 dialog.DefaultExtension = ".xlsx";
                 dialog.InitialDirectory = _IO.Directory.GetCurrentDirectory();
                 dialog.Multiselect = false;
 
+                // if file was chosen
                 if (dialog.ShowDialog() == CommonFileDialogResult.Ok && dialog.FileName != null)
                 {
                     currentPath = dialog.FileName;
@@ -71,9 +85,14 @@ namespace to_doors_app.ViewModels.Helpers
                 MessageBox.Show($"Error during setting path to mts file:\n {ex}");
             }
 
+            // return path
             return currentPath;
         }
 
+        /// <summary>
+        /// Opens open directory dialog and allows picking of output folder
+        /// </summary>
+        /// <returns>Path to chosen folder</returns>
         public static string GetOutputPath()
         {
             string directoryPath = string.Empty;
@@ -111,18 +130,25 @@ namespace to_doors_app.ViewModels.Helpers
             return moduleNamesToReturn;
         }
 
+        /// <summary>
+        /// Adds overview report path to correct ModuleToUiDto object
+        /// </summary>
+        /// <param name="itemsToAdd">list of processed objects</param>
+        /// <param name="reportsPaths">list of overview reports paths</param>
         internal static void SetOverviewReportsPathsToModules(ref List<ModuleToUiDto> itemsToAdd, List<string> reportsPaths)
         {
+            //for each path found 
             foreach (string path in reportsPaths)
             {
+                //for each module
                 foreach (ModuleToUiDto module in itemsToAdd)
                 {
-                    /* if path contains module name */
+                    //if path contains module name
                     if (path.Contains(module.Name))
                     {
-                        /* path was found */
+                        //path was found
                         module.PathToOverviewReport = path;
-                        /* go to next path*/
+                        //go to next path
                         break;
                     }
                 }
